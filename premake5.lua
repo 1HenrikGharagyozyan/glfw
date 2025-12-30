e5cd229dae131e285dbeeb257f0d1722bdaf5159
HenrikGharagyozyan1 <henrik.gharagyozyan1@email.com>
2025-12-30 22:06:17 +0400

    Add Static lib

--- premake5.lua
+++ premake5.lua
@@ -1,13 +1,12 @@
 project "GLFW"
-    kind "SharedLib"
+    kind "StaticLib"
     language "C"
-    staticruntime "On"
     
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    files
-    {
+	files
+	{
         "include/GLFW/glfw3.h",
         "include/GLFW/glfw3native.h",
         "src/glfw_config.h",
@@ -18,22 +17,21 @@ project "GLFW"
         "src/platform.c",
         "src/vulkan.c",
         "src/window.c",
-
-        -- null backend (stub-реализации)
         "src/null_init.c",
-        "src/null_joystick.c",
         "src/null_monitor.c",
-        "src/null_window.c"
+        "src/null_window.c",
+        "src/null_joystick.c"
     }
-
-    filter "system:windows"
-        systemversion "latest"
-
+    
+	filter "system:windows"
+        buildoptions { "-std=c11", "-lgdi32" }
+        systemversion "10.0.17134.0"
+        staticruntime "On"
+        
         files
         {
             "src/win32_init.c",
             "src/win32_joystick.c",
-            "src/win32_module.c",
             "src/win32_monitor.c",
             "src/win32_time.c",
             "src/win32_thread.c",
@@ -43,45 +41,46 @@ project "GLFW"
             "src/osmesa_context.c"
         }
 
-        defines 
-        { 
+		defines 
+		{ 
             "_GLFW_WIN32",
             "_CRT_SECURE_NO_WARNINGS"
-        }
-    
+		}
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
+        
     filter "system:linux"
-        pic "On"  -- важно для SharedLib
         files
         {
             "src/x11_init.c",
-            -- "src/x11_joystick.c",
             "src/x11_monitor.c",
             "src/x11_window.c",
+            "src/x11_platform.h",
             "src/xkb_unicode.c",
+            "src/xkb_unicode.h",
             "src/posix_time.c",
             "src/posix_thread.c",
+            "src/posix_time.h",
+            "src/posix_thread.h",
             "src/glx_context.c",
             "src/egl_context.c",
             "src/osmesa_context.c",
             "src/linux_joystick.c",
+            "src/linux_joystick.h",
             "src/posix_module.c",
-            "src/posix_poll.c"
-            -- "src/bsd_joystick.c" -- удалено, файла нет
+            "src/posix_poll.c",
+            "src/posix_poll.h"
         }
+
         defines 
         { 
             "_GLFW_X11"
         }
-    filter {}
-
+        
     filter "configurations:Debug"
         runtime "Debug"
-        symbols "On"
+        symbols "on"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "On"
-
-    filter "configurations:Dist"
-        runtime "Release"
-        optimize "On"
+        optimize "on"
